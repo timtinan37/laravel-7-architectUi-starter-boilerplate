55474d9d4bf3459c898d6a04df3cd7347eccd3b0
Tariq Imtinan <timtinan@outlook.com>
2021-01-06 15:24:08 +0600

    break template to smaller reusable parts

---
+++ resources/views/backend/main.blade.php
@@ -0,0 +1,17 @@
+<div class="app-page-title">
+    <div class="page-title-wrapper">
+        <div class="page-title-heading">
+            <div class="page-title-icon">
+                <i class="pe-7s-anchor icon-gradient bg-mean-fruit">
+                </i>
+            </div>
+            <div>
+                @section('page-title') Give me a Title @show
+                <div class="page-title-subheading">
+                    @section('page-title-subheading') Give me a subheading @show
+                </div>
+            </div>
+        </div>    
+    </div>
+</div>
+@yield('main')
